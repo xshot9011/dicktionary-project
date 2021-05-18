e1eb4f1c246c9e39b1e46cce72a4dfdfb5e0a35b
unknown <sedthawut2000@gmail.com>
2021-05-18 23:20:06 +0700

    update admin path

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from decouple import config
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(config('ADMIN_PATH'), admin.site.urls),
 ]
